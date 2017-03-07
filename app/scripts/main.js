7243d565f8d1d2be220f86599d9b67d500945d47
DELMON Cécile <cdelmon@p-212-spare.intra.gobelins.fr>
2017-03-07 18:41:40 +0100

    import data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('\'Allo \'Allo!');
+/////////////
+//Parse csv file
+/////////////
+d3.csv('/scripts/data.csv')
+    .row(row)
+    .get(loaded);
+
+function loaded(data) {
+  for (var i = 0; i < data.length; i++) {
+    d3.select("div.main")
+      .append("img")
+      .attr("src", data[i].img)
+      .attr("alt", data[i].title);
+  }
+  console.log('ok', data);
+
+}
+
+//formattage data
+function row(d) {
+    return {
+        title: d.title,
+        subtitle: d.subtitle,
+        date: d.date,
+        summary: d.summary,
+        img: d.img,
+        link: d.link,
+    };
+}
